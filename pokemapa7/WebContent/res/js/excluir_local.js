8f5ebaaa66b932dea71150bc7619e4ff1714b4ab
Hygor Souza <souza.hygor13@gmail.com>
2017-04-22 15:41:02 -0300

    remove

--- pokemapa7/WebContent/res/js/excluir_local.js
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-
-$("#delete-modal").on('show.bs.modal', function(event) {
-	var button = $(event.relatedTarget); // botao que disparou a modal
-	var recipient = button.data('local');
-	$("#id_excluir").val(recipient);
-});
